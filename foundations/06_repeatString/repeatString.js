099287d8b8f73ee73da96b20d72bfe6b7fae1ead
akhmadgibran <akhmadnabilg@gmail.com>
2026-03-01 14:43:33 +0700

    done foundation javascript 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let word = string;
+    let time = num;
+    let fullString = "";
 
+    if (time < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < time; i++) {
+        fullString += word;
+    }
+
+    return fullString;
 };
 
 // Do not edit below this line

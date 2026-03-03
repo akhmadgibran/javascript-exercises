fcca440344e1b7e12f1840972fc58b133518040b
akhmadgibran <akhmadnabilg@proton.me>
2026-03-03 20:38:15 +0700

    done: foundation javascript 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayInput, ...args) {
+    // new array
+    const newArray = [];
+
+    arrayInput.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line

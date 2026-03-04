6457f89460a3de5c539e6ebc8131f8ea484f20dc
akhmadgibran <akhmadnabilg@gmail.com>
2026-03-04 10:40:44 +0700

    done: foundation javascript 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+// tahun kabisat
+// apabila tahun bisa dibagi oleh 4 maka itu kabisat
+// apabila habis dibagi 100 dia bukan kabisat, kecuali bisa dibagi oleh 400
 
+const leapYears = function (year) {
+    // declaring the condition
+    const DivisibleByFour = year % 4 === 0;
+    const Century = year % 100 === 0;
+    const DivisibleByFourHundred = year % 400 === 0;
+
+    if (DivisibleByFour && (!Century || DivisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
